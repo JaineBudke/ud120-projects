2c8dd74472eb286d3f420b19cf0fe78fbbac1d5c
jainebudke <jainebudke@hotmail.com>
2019-02-14 10:46:37 -0300

    naive bayes module complete

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,7 +9,8 @@
     Sara has label 0
     Chris has label 1
 """
-    
+  
+from sklearn.naive_bayes import GaussianNB  
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,6 +27,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = GaussianNB()
+
+t0 = time()
+
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+
+tt0 = time()
+clf.predict(features_test)
+print "prediction time:", round(time()-tt0, 3), "s"
+
+
+print clf.score(features_test, labels_test)
 
 
 #########################################################
